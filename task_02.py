33e40889f6346d2ab5e2b8284fcf92aef3a76888
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-23 01:26:00 +0000

    Wk4 Synth T4

--- task_02.py
+++ task_02.py
@@ -6,12 +6,11 @@
 DAY_CHECK = ['SAT', 'sat', 'SATURDAY', 'saturday'
              'SUN', 'sun', 'SUNDAY', 'sunday']      # weekends check variable
 DAY = raw_input('PLEASE ENTER THE DAY ')
-SNOOZE = bool                                       # Setting a boolean variable
 
 TIME_NEW = int(raw_input('PLEASE ENTER THE *4* DIGITS MILITARY TIME '))
 
-if DAY in DAY_CHECK or TIME_NEW < 600:       # if wknd variable or <600
-    SNOOZE = True
+if DAY is DAY_CHECK or TIME_NEW < 600:       # if wknd variable or <600
+    SNOOZE = True                            # Setting a boolean variable
 
 else:
     print 'Beep! ' * 5
